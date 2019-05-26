5066c3132796bbcc4542a562f56c140a6e4e5f67
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2019-05-26 20:38:38 +0200

    beginning of MaxFlow mincost matching

--- src/main/java/louchtch/graphmatch/matching/Matching.java
+++ src/main/java/louchtch/graphmatch/matching/Matching.java
@@ -2,5 +2,8 @@ package louchtch.graphmatch.matching;
 
 public interface Matching
 {
-
+	/**
+	 *  stub for returning the matching -- need to determine the interface of Matching first ...
+	 */
+	Object stuff();
 }
--- src/main/java/louchtch/graphmatch/matching/MaxFlowMatching.java
+++ src/main/java/louchtch/graphmatch/matching/MaxFlowMatching.java
@@ -1,12 +1,46 @@
 package louchtch.graphmatch.matching;
 
-import louchtch.graphmatch.model.Graph;
+import louchtch.graphmatch.model.MaxFlowGraph;
 import louchtch.graphmatch.model.SearchType;
 
-public class MaxFlowMatching implements Matching
+import java.util.Optional;
+
+public class MaxFlowMatching<T> implements Matching
 {
-	public <T> MaxFlowMatching(Graph<T> graph, SearchType searchType)
+	private final MaxFlowGraph<T> graph;
+	private final SearchType searchType;
+
+	public MaxFlowMatching(MaxFlowGraph<T> graph, SearchType searchType)
 	{
-		super();
+		this.graph = graph;
+		this.searchType = searchType;
+	}
+
+	@Override
+	public Object stuff()
+	{
+		return null;
+	}
+
+	private Optional<Path> findAugmentingPath()
+	{
+		// searh type distinction -- todo: move to own implementations of MaxFlowMatching instead
+		// todo: implement shortest path or remove from SearchType enum
+//		if (searchType == SearchType.ShortestPath)
+//		{
+//
+//		}
+
+		//
+		if (searchType == SearchType.MinCost)
+		{
+
+		}
+
+		throw new RuntimeException("Unsupported search type: " + searchType);
+	}
+
+	private class Path
+	{
 	}
 }

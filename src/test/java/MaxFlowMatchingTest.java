efa4053c34f7bd4f46fe44fcfca7f86a8eb95067
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2019-05-31 11:41:32 +0200

    turn simple test scenario into proper test

--- src/test/java/MaxFlowMatchingTest.java
+++ src/test/java/MaxFlowMatchingTest.java
@@ -1,19 +1,21 @@
 import louchtch.graphmatch.matching.MaxFlowMatching;
 import louchtch.graphmatch.model.*;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.*;
+
 public class MaxFlowMatchingTest
 {
 	@Test
-	public void asListOfEdges()
+	public void simpleMatchingScenario()
 	{
-		Vertices<String> left = new Vertices<>();
-		Vertices<String> right = new Vertices<>();
+		DirectedWeightedEdges<String> edges = new DirectedWeightedEdges<>();
 
-
-		// poeple
+		// people
 		Vertex<String> p_a = new Vertex<>("a");
 		Vertex<String> p_b = new Vertex<>("b");
 		Vertex<String> p_c = new Vertex<>("c");
@@ -27,31 +29,42 @@ public class MaxFlowMatchingTest
 		Vertex<String> t_4 = new Vertex<>("4");
 		Vertex<String> t_5 = new Vertex<>("5");
 
-		DirectedWeightedEdges<String> edges = new DirectedWeightedEdges<>();
-
+		// A can do 1, 2
 		edges.add(p_a.makeEdgeTo(t_1, 1));
 		edges.add(p_a.makeEdgeTo(t_2, 1));
 
+		// B only 1
 		edges.add(p_b.makeEdgeTo(t_1, 1));
 
+		// C can do 2, 3
 		edges.add(p_c.makeEdgeTo(t_2, 1));
 		edges.add(p_c.makeEdgeTo(t_3, 1));
 
+		// D can do 3, 4, 5
 		edges.add(p_d.makeEdgeTo(t_3, 1));
 		edges.add(p_d.makeEdgeTo(t_4, 1));
 		edges.add(p_d.makeEdgeTo(t_5, 1));
 
+		// E only 5
 		edges.add(p_e.makeEdgeTo(t_5, 1));
 
-		MaxFlowGraph<String> graph = new MaxFlowGraph<>(
-			left.with(p_a).with(p_b).with(p_c).with(p_d).with(p_e),
-			right.with(t_1).with(t_2).with(t_3).with(t_4).with(t_5),
-			edges
-		);
+
+		Vertices<String> left = new Vertices<String>().with(p_a).with(p_b).with(p_c).with(p_d).with(p_e);
+		Vertices<String> right = new Vertices<String>().with(t_1).with(t_2).with(t_3).with(t_4).with(t_5);
+
+		MaxFlowGraph<String> graph = new MaxFlowGraph<>(left, right, edges);
 
 		MaxFlowMatching<String> matching = new MaxFlowMatching<>(graph, SearchType.MinCost);
 		List<Edge<String>> matchingAslist = matching.asListOfEdges();
 
-		System.out.println("done");
+		Assert.assertThat(matchingAslist, hasItems(
+			Edge.between(p_b, t_1),
+			Edge.between(p_a, t_2),
+			Edge.between(p_c, t_3),
+			Edge.between(p_d, t_4),
+			Edge.between(p_e, t_5)
+		));
+
+		Assert.assertEquals(5, matchingAslist.size());
 	}
 }
